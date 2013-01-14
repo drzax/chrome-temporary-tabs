abf541bc0d9e65ea7cead70d754a70b5484fed52
Simon Elvery <simon@elvery.net>
2013-01-15 07:58:12 +1000

    Starting conversion to self destruct by default.

--- background.js
+++ background.js
@@ -1,59 +1,57 @@
 var loopTimeout = 0;
+var tabs = [];
 
-function loop(){
+(function loop(){
 	loopTimeout = setTimeout(function(){
 
-		chrome.idle.queryState(60,function(state){
-			
-			console.log('timeout');
+		chrome.idle.queryState(15,function(state){
 			
 			// Clean up temporary tabs
 			if (state === 'idle') {
-				chrome.storage.local.get({'temporaryTab':[]}, function(data){
-					chrome.tabs.remove(data.temporaryTab);
-				});
-				chrome.storage.local.set({'temporaryTab':[]});
-				clearTimeout(loopTimeout);
-				loopTimeout = 0;
+				chrome.tabs.remove(tabs);
 			}
 		});
 
 		// Loop
 		loop();
 	},1000);
-};
+})();
 
-function makeTemporary(tab) {
-	chrome.storage.local.get({'temporaryTab':[]}, function(data){
-		data.temporaryTab.push(tab.id);
-		chrome.storage.local.set({'temporaryTab':data.temporaryTab});
-		if (loopTimeout === 0) loop();
-	});
+function defuse(tab) {
+	var idx = tabs.indexOf(tab.id);
+	if (idx >= 0) {
+		tabs.splice(idx,1);
+	}
+	console.log(tabs);
+	
+//	chrome.storage.local.get({'temporaryTab':[]}, function(data){
+//		data.temporaryTab.push(tab.id);
+//		chrome.storage.local.set({'temporaryTab':data.temporaryTab});
+//		if (loopTimeout === 0) loop();
+//	});
 }
 
-chrome.commands.onCommand.addListener(function(command) {
-	if (command === "temporaryTab") {
-		chrome.tabs.create( {}, makeTemporary);
-	}
-	
-	if (command === "selfDestruct") {
-		chrome.tabs.getCurrent(makeTemporary);
-	}
+chrome.tabs.onUpdated.addListener(function(tabId, info, tab){
+	chrome.pageAction.show(tabId);
 });
 
 chrome.tabs.onRemoved.addListener(function(tabId, info){
-	console.log(info);
-	chrome.storage.local.get({'temporaryTab':[]}, function(data){
-		var idx = data.temporaryTab.indexOf(tabId);
-		if (idx >= 0) {
-			data.temporaryTab.splice(idx,1);
-			chrome.storage.local.set({'temporaryTab':data.temporaryTab});
-			
-			if (data.temporaryTab.length === 0) {
-				clearTimeout(loopTimeout);
-				loopTimeout = 0;
-			}
-		}
-	});
+	var idx = tabs.indexOf(tabId);
+	if (idx >= 0) {
+		tabs.splice(idx,1);
+	}
+	
+//	chrome.storage.local.get({'temporaryTab':[]}, function(data){
+//		var idx = data.temporaryTab.indexOf(tabId);
+//		if (idx >= 0) {
+//			data.temporaryTab.splice(idx,1);
+//			chrome.storage.local.set({'temporaryTab':data.temporaryTab});
+//			
+//			if (data.temporaryTab.length === 0) {
+//				clearTimeout(loopTimeout);
+//				loopTimeout = 0;
+//			}
+//		}
+//	});
 });
 
