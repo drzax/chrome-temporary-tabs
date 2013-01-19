001c9efe3ff8f0804353590138897be62802cbe9
Simon Elvery <simon@elvery.net>
2013-01-20 09:56:59 +1000

    Popup improvements.

--- popup.js
+++ popup.js
@@ -38,28 +38,32 @@
 	chrome.storage.local.get({removed: []}, function(data) {
 		var i, date, lastDate, currentDay, table='';
 		
-		for (i in data.removed) {
-			
-			// Check for valid date
-			if (isNaN(Date.parse(data.removed[i].removed))) continue;
-			
-			date = new Date(data.removed[i].removed);
-			
-			if (currentDay !== date.toDateString()) {
-				if (table !== null) table += '</ol>';
-				table += getDateHeading(date);
-				table += '<ol class="history">';
-				currentDay = date.toDateString();
+		if (data.removed.length) {
+			for (i in data.removed) {
+
+				// Check for valid date
+				if (isNaN(Date.parse(data.removed[i].removed))) continue;
+
+				date = new Date(data.removed[i].removed);
+
+				if (currentDay !== date.toDateString()) {
+					if (table !== null) table += '</ol>';
+					table += getDateHeading(date);
+					table += '<ol class="history">';
+					currentDay = date.toDateString();
+				} else if (lastDate && (lastDate-date)/1000/60 > 20) {
+					table += '<li class="divider"><span class="time">|</span></li>';
+				}
+				table += '<li><span class="time">' + date.get12HourTime() + '</span> ';
+				table += '<img width="16" height="16" src="' + data.removed[i].favIconUrl + '"/>';
+				table += '<a href="' + data.removed[i].url + '">' + ((data.removed[i].title.length > 50) ? data.removed[i].title.substr(0, 50) + '&hellip;' : data.removed[i].title) + '</a></li>';
+				lastDate = date;
 			}
-			if (lastDate && (lastDate-date)/1000/60 > 20) {
-				table += '<li class="divider"><span class="time">|</span></li>';
-			}
-			table += '<li><span class="time">' + date.get12HourTime() + '</span> ';
-			table += '<img width="16" height="16" src="' + data.removed[i].favIconUrl + '"/>';
-			table += '<a href="' + data.removed[i].url + '">' + ((data.removed[i].title.length > 50) ? data.removed[i].title.substr(0, 50) + '&hellip;' : data.removed[i].title) + '</a></li>';
-			lastDate = date;
+			table += '</ol>';
+		} else {
+			table = '<div class="nothing">Still waiting for <br>an explosion</div>';
 		}
-		table += '</ol>';
+			
 		$('#list').html(table);
 	});
 	
@@ -74,7 +78,12 @@
 	}, function(tabs){
 		var registry = chrome.extension.getBackgroundPage().TabRegistry,
 			guid = (tabs.length) ? registry.guid(tabs[0].id) : null;
-		if (registry.get(guid, 'defused')) $('#defuse').attr('disabled','disabled').text('Defused');
+		if (guid) {
+			if (registry.get(guid, 'defused')) $('#defuse').attr('disabled','disabled').text('Defused');
+		} else {
+			$('#defuse').hide();
+		}
+		
 	});
 	
 	$('#list').on('click', 'a', function(){
